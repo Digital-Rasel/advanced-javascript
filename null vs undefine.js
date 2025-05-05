29df1298881f58f624ea9646535a2f43ce5d4ec3
Digital-Rasel <raselahmmedbaf@gmail.com>
2025-05-05 22:04:46 +0600

    null vs undefined cleared for interview

---
+++ null vs undefine.js
@@ -0,0 +1,13 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add (num1, num2) {
+//     console.log(num1 + num2);
+// }
+// function add (num1, num2) {
+//     console.log(num2);
+// }
+// const premik = {name: "smart dude" , phone: 12345};
+let fun = undefined;
+
+console.log(fun);
